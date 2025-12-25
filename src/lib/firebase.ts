1a2ebbbed8d19cea2fe0e1c22dece05db4123d8e
jawwadkareem <karimjawwad09@gmail.com>
2025-12-25 19:23:09 +0500

    UI updated

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk559QWXeyh7uLddSZ-jL9z0Fk57scQ3U",
-  authDomain: "tutorspooldb.firebaseapp.com",
-  projectId: "tutorspooldb",
-  storageBucket: "tutorspooldb.firebasestorage.app",
-  messagingSenderId: "1012964928731",
-  appId: "1:1012964928731:web:76f2f66d93d39944ccf0fe",
-  measurementId: "G-ZKMSQ1TGHT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
